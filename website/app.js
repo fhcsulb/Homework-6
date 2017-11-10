3e9920c3be62b0cec8218e5f8953f9549c727474
Felix <Felix.Huang@student.csulb.edu>
2017-11-09 21:57:30 -0800

    Delete button works!

--- website/app.js
+++ website/app.js
@@ -1,67 +1,61 @@
 const  candidates = [
-    { name: 'Peter Parker', likes: 700},
-    { name: 'Tony Stark', likes: 135},
-    { name: 'dash', likes: 0},
-  ]
-
-  
-  function printCandidates()
-  {
-      for ( i = 0; i < candidates.length; i++)
-      {
-        
-
-            let element = document.createElement("div");
-            document.getElementById("candidate").appendChild(element);
-
-            let candidate_name = document.createElement("p");
-            candidate_name.innerHTML = candidates[i].name;
-            element.appendChild(candidate_name);
-
-            let like_count = document.createElement("p");
-
-            // set the like counter ID to the array index.
-            like_count.id=i;
-            like_count.innerHTML = candidates[i].likes;
-            element.appendChild(like_count);
-            
-
-            let like_button = document.createElement("button");
-            like_button.innerHTML = "Like";
-            like_button.className=i;
-            like_button.onclick = function(){ likeIncrement(this.className)};
-            element.appendChild(like_button);
-
-            let delete_button = document.createElement("button");
-            delete_button.innerHTML = "Delete";
-            delete_button.onclick = function () { deleteCandidate(this.className)};
-            
-            element.appendChild(delete_button);
-
-      }
-  }
-
- 
+  { name: 'Peter Parker', likes: 700},
+  { name: 'Tony Stark', likes: 135},
+  { name: 'dash', likes: 0},
+]
 
 
-  function deleteCandidate(index)
-  {
-      console.log(index);
-      var child = document.getElementsByClassName(index);
-      console.log(child);
-      child.parentNode.removeChild(child);
-      console.log("Delete Button clicked ! ");
-  }
-    
-     
-  function likeIncrement(index)
-  {
-      console.log(candidates[index]);
+function printCandidates()
+{
+    for ( i = 0; i < candidates.length; i++)
+    {
       
-      candidates[index].likes = candidates[index].likes+1;
-      document.getElementById(index).innerHTML = candidates[index].likes;
-      console.log("Like Button clicked ! ");
-  }
+
+          let element = document.createElement("div");
+          document.getElementById("candidate").appendChild(element);
+        
+          element.id = candidates[i].name;
+          let candidate_name = document.createElement("p");
+          candidate_name.innerHTML = candidates[i].name;
+          element.appendChild(candidate_name);
+
+          let like_count = document.createElement("p");
+
+          // set the like counter ID to the array index.
+          like_count.id=i;
+          like_count.innerHTML = candidates[i].likes;
+          element.appendChild(like_count);
+          
+
+          let like_button = document.createElement("button");
+          like_button.innerHTML = "Like";
+          like_button.className=i;
+          like_button.onclick = function(){ likeIncrement(this.className)};
+          element.appendChild(like_button);
+
+          let delete_button = document.createElement("button");
+          delete_button.innerHTML = "Delete";
+          delete_button.className = i;
+          delete_button.onclick = function(){ deleteCandidate(this.className);};
+          element.appendChild(delete_button);
+
+
+    }
+}
+
+
+
+
+function deleteCandidate(index)
+{
+  console.log(index);
+  var parent = document.getElementById("candidate");
+  let child = document.getElementById(candidates[index].name);
+  console.log(child);
+  parent.removeChild(child);
+  console.log("Delete Button clicked ! ");
+}
+
 
 
   function addCandidate()
@@ -70,6 +64,7 @@ const  candidates = [
     candidates.push({name: new_name, likes: 0 })
 
     let element = document.createElement("div");
+    element.id = new_name;
     document.getElementById("candidate").appendChild(element);
 
     let candidate_name = document.createElement("p");
@@ -89,14 +84,19 @@ const  candidates = [
     element.appendChild(like_button);
 
     let delete_button = document.createElement("button");
+    delete_button.className = candidates.length - 1;
     delete_button.innerHTML = "Delete";
     delete_button.onclick = function () { deleteCandidate(this.className)};
     
     element.appendChild(delete_button);
+  }
 
-
-
-
-    
-    
+       
+  function likeIncrement(index)
+  {
+      console.log(candidates[index]);
+      
+      candidates[index].likes = candidates[index].likes+1;
+      document.getElementById(index).innerHTML = candidates[index].likes;
+      console.log("Like Button clicked ! ");
   }
